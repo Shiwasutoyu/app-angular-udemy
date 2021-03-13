53759aed4c1c02149a69ea7bccc4470484fc9a9c
MathiasGK <lemillionwork426@gmail.com>
2021-03-13 21:44:26 +0900

    フロントエンド側作成

---
+++ src/app/products.ts
@@ -0,0 +1,42 @@
+export const products = [
+  {
+    coverImage: './assets/img/phone-cover.jpg',
+    name:'Phone XL',
+    price:799,
+    description:'A large phone with one of the best screens',
+    heading1:'サンプルサンプル1',
+    heading2:'サンプルサンプル2',
+    heading3:'サンプルサンプル3',
+    headingtext1:'サンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプル',
+    headingtext2:'SampleSampleSampleSampleSampleSampleSampleSampleSampleSampleSampleSampleSampleSampleSampleSample',
+    headingtext3:'Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.',
+  },
+  {
+    coverImage: './assets/img/phone-cover.jpg',
+    name:'Phone Mini',
+    price:699,
+    description:'A great phone with one of the best cameras',
+    heading1:'サンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプル1',
+    heading2:'サンプルサンプル2',
+    heading3:'サンプルサンプル3',
+    
+  },
+  {
+    coverImage: './assets/img/phone-cover.jpg',
+    name:'Phone Standard',
+    price:299,
+    description:'A great phone with one of the best cameras',
+    heading1:'サンプルサンプル1',
+    heading2:'サンプルサンプル2',
+    heading3:'サンプルサンプル3'
+  },
+  {
+    coverImage: './assets/img/phone-cover.jpg',
+    name:'Phone Special',
+    price:999,
+    description:'',
+    heading1:'サンプルサンプル1',
+    heading2:'サンプルサンプル2',
+    heading3:'サンプルサンプル3'
+  }
+];
